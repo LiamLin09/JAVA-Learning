8e030c28d42a04e973bc86123a2228f45c10e155
LiamLin <137009133+LiamLin09@users.noreply.github.com>
2023-08-16 09:49:21 -0400

    Add files via upload

---
+++ 2. Expressions & statements & more/statement white space and indenting/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myVariable = 50;
+
+        myVariable++;
+        myVariable--;
+
+        System.out.println("This is a test!!!!");
+
+        System.out.println("This is" +
+                "another" +
+                "still more");
+
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("myVariable = " + myVariable);
+    }
+}
