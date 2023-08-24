14c978a936dd70210b7c4e0e1ea90a7d32468fa9
LiamLin <137009133+LiamLin09@users.noreply.github.com>
2023-08-24 14:22:23 -0400

    Add files via upload

---
+++ 4. OPP Part 1 - Inheritance/Inheritance/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Generic Animal", "Huge", 400);
+        doAnimalStuff(animal, "slow");
+
+        Dog dog = new Dog();
+        doAnimalStuff(dog, "fast");
+
+        Dog yourkie = new Dog("Yorkie", 15);
+        doAnimalStuff(yourkie, "fast");
+        Dog retriever = new Dog("Labrador Retriever", 65, "Floppy", "Swimmer");
+        doAnimalStuff(retriever, "slow");
+
+        Dog wolf = new Dog("Wolf", 40);
+        doAnimalStuff(wolf, "slow");
+
+        Fish goldie = new Fish("Goldfish", 0.25, 2, 3);
+        doAnimalStuff(goldie, "fast");
+    }
+
+    public static void doAnimalStuff(Animal animal, String speed) {
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_____");
+    }
+}
